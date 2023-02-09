4345ec93b3062e8a1a067360c47ec1974e7e6905
“Wangdaijie” <2796866889@qq.com>
2023-02-09 17:33:12 +0800

    2023-02-09 17:33

---
+++ 生命周期使用方法/index.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  methods: {
+    async fetchPosts() {
+      const res = await fetch("./posts.json");
+      const postsData = await res.json();
+      this.posts = postsData;
+    },
+  },
+  created() {
+    this.fetchPosts();
+  },
+});
+
+const vm = app.mount("#app");
