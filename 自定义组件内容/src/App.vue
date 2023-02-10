1c0a3c8f756956390b93459d66458f337762ce3f
“Wangdaijie” <2796866889@qq.com>
2023-02-10 20:17:32 +0800

    2023-02-10 20:17

---
+++ 自定义组件内容/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import MessageItem from "./components/MessageItem.vue";
+export default {
+  components: {
+    MessageItem,
+  },
+};
+</script>
+
+<template>
+  <main>
+    <div>
+      <MessageItem sender="张三" msg="你好" time="20:01" />
+      <MessageItem sender="张三" msg="你好" time="20:01" />
+      <MessageItem sender="张三" msg="你好" time="20:01" />
+    </div>
+  </main>
+</template>
---
+++ 自定义组件内容/src/components/MessageItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>{{ sender }}</p>
+    <p>{{ time }}</p>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["sender", "msg", "time"],
+  mounted() {
+    console.log(this.sender);
+  },
+};
+</script>
+
+<style scoped>
+div {
+  display: grid;
+  grid-template-areas:
+    "sender time"
+    "msg msg";
+  border: 1px solid hsl(280deg, 50%, 50%);
+  border-radius: 4px;
+  padding: 1em;
+  width: 250px;
+  margin-bottom: 1px;
+}
+</style>
