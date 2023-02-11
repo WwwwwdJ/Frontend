d4247d39f70ead6bc4b772d1f2d720f4059166e1
WwwwwdJ <WwwwwdJ@foxmail.com>
2023-02-11 15:58:42 +0800

    2023-02-11 15:58

---
+++ 组件数据的流向设计/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <main>
+    <div>
+      <!-- <ButtonWithProps :onclick="handleClick" /> -->
+      <ButtonWithProps />
+      <!-- <ButtonWithEmits @customClick="handleClick" /> -->
+      <ButtonWithEmits />
+    </div>
+  </main>
+</template>
+
+<script>
+import ButtonWithProps from "./components/ButtonWithProps.vue";
+import ButtonWithEmits from "./components/ButtonWithEmits.vue";
+
+export default {
+  components: {
+    ButtonWithProps,
+    ButtonWithEmits,
+  },
+  methods: {
+    handleClick(value) {
+      alert(value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ 组件数据的流向设计/src/components/ButtonWithProps.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="onclick && onclick('点击了Props按钮')">按钮</button>
+</template>
+
+<script>
+export default {
+  props: ["onclick"],
+};
+</script>
+
+<style scoped>
+button {
+  border: none;
+  background: linear-gradient(
+    90deg,
+    hsl(240deg, 50%, 50%),
+    hsl(280deg, 50%, 50%)
+  );
+  padding: 1em 2em;
+  margin-top: 24px;
+  margin-right: 12px;
+  border-radius: 4px;
+  color: white;
+}
+</style>
