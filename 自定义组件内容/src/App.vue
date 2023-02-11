a485afb898a2adebc964820dfc493c2f9bb475d6
“Wangdaijie” <2796866889@qq.com>
2023-02-11 10:19:13 +0800

    2023-02-11 10:19

--- 自定义组件内容/src/App.vue
+++ 自定义组件内容/src/App.vue
@@ -1,18 +1,44 @@
+<template>
+  <main>
+    <div>
+      <MessageItem
+        v-for="message in messages"
+        :key="message.id"
+        v-bind="message"
+      />
+    </div>
+  </main>
+</template>
+
 <script>
 import MessageItem from "./components/MessageItem.vue";
 export default {
   components: {
     MessageItem,
   },
+  data() {
+    return {
+      messages: [
+        {
+          id: 1,
+          sender: "张三",
+          msg: "你好",
+          time: "20:01",
+        },
+        {
+          id: 2,
+          sender: "张三",
+          msg: "你好",
+          time: "20:01",
+        },
+        {
+          id: 3,
+          sender: "张三",
+          msg: "你好",
+          time: "20:01",
+        },
+      ],
+    };
+  },
 };
 </script>
-
-<template>
-  <main>
-    <div>
-      <MessageItem sender="张三" msg="你好" time="20:01" />
-      <MessageItem sender="张三" msg="你好" time="20:01" />
-      <MessageItem sender="张三" msg="你好" time="20:01" />
-    </div>
-  </main>
-</template>
