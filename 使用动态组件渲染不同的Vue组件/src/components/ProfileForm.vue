586e655c91f1230a3da93e7017ec924afe1df7ce
WwwwwdJ <WwwwwdJ@foxmail.com>
2023-02-16 13:50:07 +0800

    2023-02-16 13:50

---
+++ 使用动态组件渲染不同的Vue组件/src/components/ProfileForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <form @submit.prevent>
+    <label>昵称: <input type="text" /></label>
+    <label>生日: <input type="data" /></label>
+    <label>地址: <input type="text" /></label>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+::-webkit-calendar-picker-indicator {
+  filter: invert(1);
+}
+</style>
---
+++ 使用动态组件渲染不同的Vue组件/src/components/RegisterForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <form @submit.prevent>
+    <label> 手机号:<input type="number" /> </label>
+    <label>
+      验证码:<input type="number" />
+      <button class="sendSMSCodeBtn">发送验证码</button>
+    </label>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.sendSMSCodeBtn {
+  margin-left: 24px;
+}
+</style>
