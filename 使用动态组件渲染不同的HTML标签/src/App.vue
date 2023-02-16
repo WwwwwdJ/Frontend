407e77571a6d387b0bbb6a555fdf6aa154f123b9
WwwwwdJ <WwwwwdJ@foxmail.com>
2023-02-16 11:19:36 +0800

    2023-02-16

---
+++ 使用动态组件渲染不同的HTML标签/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import TextHeading from "./components/TextHeading.vue";
+export default {
+  components: {
+    TextHeading,
+  },
+};
+</script>
+
+<template>
+  <main>
+    <div>
+      <TextHeading level="1">一级标题</TextHeading>
+      <TextHeading level="2">二级标题</TextHeading>
+      <TextHeading level="3">三级标题</TextHeading>
+      <TextHeading level="4">四级标题</TextHeading>
+      <TextHeading level="5">五级标题</TextHeading>
+      <TextHeading level="6">六级标题</TextHeading>
+    </div>
+  </main>
+</template>
