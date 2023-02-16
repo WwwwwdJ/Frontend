23abc91a4c2e8f34cf16ab431890cb405bef4246
WwwwwdJ <WwwwwdJ@foxmail.com>
2023-02-16 13:50:07 +0800

    2023-02-16 13:50

---
+++ 使用动态组件渲染不同的Vue组件/src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import RegisterForm from "./components/RegisterForm.vue";
+import ProfileForm from "./components/ProfileForm.vue";
+export default {
+  components: {
+    RegisterForm,
+    ProfileForm,
+  },
+  data() {
+    return {
+      currentForm: "RegisterForm",
+    };
+  },
+};
+</script>
+
+<template>
+  <main>
+    <div>
+      <component :is="currentForm" />
+      <div class="buttons">
+        <button
+          v-if="currentForm === 'RegisterForm'"
+          @click="currentForm = 'ProfileForm'"
+        >
+          下一步
+        </button>
+        <template v-if="currentForm === 'ProfileForm'">
+          <button @click="currentForm = 'RegisterForm'">上一步</button>
+          <button>完成</button>
+        </template>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style></style>
